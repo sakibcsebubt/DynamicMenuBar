bdede37cc9346108841dc84e3eb839d0f118042d
SAKIB HASAN <94765148+sakibcsebubt@users.noreply.github.com>
2022-11-26 14:59:05 +0600

    Initial Commit By Sakib#51

---
+++ DynamicMenu/Controllers/MenusController.cs
@@ -0,0 +1,51 @@
+﻿using DynamicMenubar.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynamicMenubar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenusController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Test()
+        {
+            List<ActionModel> ActionList = new List<ActionModel>()
+{
+    new ActionModel() { Id = 1, ActionName = "NoAction" },
+    new ActionModel() { Id = 1, ActionName = "Create" },
+    new ActionModel() { Id = 1, ActionName = "Edit" },
+};
+            List<MenusModel> AllMenuList = new List<MenusModel>()
+{
+    new MenusModel() { MenuId = 1, MenuName = "Google", ParentMenuId = -1 },
+    new MenusModel() { MenuId = 2, MenuName = "Microsoft", ParentMenuId = -1 },
+    new MenusModel() { MenuId = 3, MenuName = "Oracle", ParentMenuId = -1 },
+    new MenusModel() { MenuId = 4, MenuName = "Gmail", ParentMenuId = 1 },
+    new MenusModel() { MenuId = 5, MenuName = "Sheets", ParentMenuId = 1 },
+    new MenusModel() { MenuId = 6, MenuName = "Adsense", ParentMenuId = 1 },
+    new MenusModel() { MenuId = 7, MenuName = "Azure", ParentMenuId = 2 },
+    new MenusModel() { MenuId = 8, MenuName = "SharePoint", ParentMenuId = 2 },
+    new MenusModel() { MenuId = 9, MenuName = "Office", ParentMenuId = 2 },
+    new MenusModel() { MenuId = 10, MenuName = "Java", ParentMenuId = 3 },
+    new MenusModel() { MenuId = 11, MenuName = "Word", ParentMenuId = 9 },
+    new MenusModel() { MenuId = 12, MenuName = "Excel", ParentMenuId = 9 },
+    new MenusModel() { MenuId = 13, MenuName = "PowerPoint", ParentMenuId = 9 },
+};
+            var AllMenuList1 = AllMenuList;
+            var navigationItems = AllMenuList1.Select(i => new NavigationItem { MenuId = i.MenuId, MenuName = i.MenuName, ParentMenuId = i.ParentMenuId }
+).ToList();
+
+            foreach (var i in navigationItems)
+            {
+                i.Children = navigationItems.Where(n => n.ParentMenuId == i.MenuId).ToList();
+                i.ActionList = ActionList;
+            }            
+
+            var rootNavigationItems = navigationItems.Where(n => n.ParentMenuId == -1);
+            var test = rootNavigationItems.ToList();
+            return Ok(rootNavigationItems.ToList());
+        }
+    }
+}
---
+++ DynamicMenu/Models/MenusModel.cs
@@ -0,0 +1,26 @@
+﻿namespace DynamicMenubar.Models
+{
+    public class MenusModel
+    {
+        public long MenuId { get; set; }
+        public string? MenuName { get; set; }
+        public long ParentMenuId { get; set; }
+    }
+    public class ActionModel
+    {
+        public long Id { get; set; }
+        public string? ActionName { get; set; }
+        public bool IsSelected { get; set; }
+    }
+  
+
+    public class NavigationItem
+    {
+        public long MenuId { get; set; }
+        public string MenuName { get; set; }
+        public long ParentMenuId { get; set; }
+        public List<NavigationItem> Children { get; set; }
+        public List<ActionModel> ActionList { get; set; }
+    }
+
+}
